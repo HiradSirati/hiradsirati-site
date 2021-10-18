31120004dec4061cda2b0f530a4ace3c44361aab
HiradSirati <84270230+HiradSirati@users.noreply.github.com>
2021-10-18 23:49:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+  server.bind(("https://hiradsirati-site.herokuapp.com/", 8000))
+  server.listen()
+  server_is_on = True
+  while server_is_on:
+    connection, address = server.accept()
